821541230e0382c559606da62c2f83c30e09b87a
yangeng2 <yangeng2@a2e70646-4c9d-4ed2-889b-a337aa36896b>
2015-02-03 20:09:34 +0000

    fake submssion

--- fake.c
+++ fake.c
@@ -13,7 +13,7 @@ int main(int argc, char * argv[]){
 	
 	FILE * ff;
 	char **content = (char ** )malloc(100 * sizeof(char *));
-	//job_ptr j = calloc(1, sizeof(struct job));
+	
 	ff = fopen("Fakefile.txt", "r+");
 	if (argc < 2)
 	{
@@ -25,6 +25,7 @@ int main(int argc, char * argv[]){
 	strcpy(name, argv[1]);
 	//printf("%d", strlen(name));
 	int i;
+	int count = 0;
 	for(i = 0; i < 100; i++){
 		if(!feof(ff)){
 			char line[2048];
@@ -32,13 +33,16 @@ int main(int argc, char * argv[]){
 				//printf("%s", line);
 				content[i] = (char *)malloc(2048 * sizeof(char));
 				strcpy(content[i], line);
+				count++;
 				//printf("%s", content[i]);
 			}
 			
 		}
+		/**
 		else{
 			break;
 		}
+		*/
 	}
 	
 	
@@ -47,17 +51,20 @@ int main(int argc, char * argv[]){
 	int index = -1;
 	
 	int k;
-	for(k = 0; k < 100; k ++){
+	//printf("find\n");
+	for(k = 0; k < count; k ++){
 		
 		if(strlen(name) == (strlen(content[k]) - 2)){
 			//printf("aaa");
 			if(strstr(content[k], name) != NULL){
+
 				index = k;
 				printf("Found %s target\n", argv[1]);
 				break;
 			}
 		}
 	}
+	
 	if(index == -1){
 		printf("Cannot build %s\n", argv[1]);
 		exit(1);
@@ -67,65 +74,59 @@ int main(int argc, char * argv[]){
 
 	while(content[k][0] == ' '){
 		printf("Running%s\n", content[k]);
-			
+		char temp[strlen(content[k]) + 1];
+		strcpy(temp, content[k]);
 		char ** arg = NULL;
-		char * p = strtok(content[k], " \"");
+		char * p = strtok(content[k], " \"");//
+		//printf("debug %s\n", content[k]);
 		int m = 0;
 		while(p){
 			arg = realloc(arg, sizeof(char *) * ++m);
 			arg[m-1] = p;
-			p = strtok(NULL, " \"");
+			p = strtok(NULL, " \"");//
 			
 		}
 			
 		arg = realloc(arg, sizeof(char*)*(m+1));
 		arg[m] = NULL;
-		execute(arg);
-		k++;
-	}
-	//printf("%s was built successfully! \n", name);
-	exit(0);
-	/**
-	if(pid == -1){
-		exit(1);
-	}
-	if(pid > 0){
-		int status;
-		while( waitpid(pid, &status, 0) != pid);
-		//printf("%d %d\n", in, pid);
-		printf("%s was built successfully! \n", name);
-		exit(0);
-	
-	}
-	if(pid == 0){
-		//while(content[k][0] == ' '){
-			printf("Running%s\n", content[k]);
+		/**
+		if(content[k][1] == '.'){
+			char ** temp = NULL;
 			
-			char ** arg = NULL;
-			char * p = strtok(content[k], " \"");
-			int m = 0;
-			while(p){
-				arg = realloc(arg, sizeof(char *) * ++m);
-				arg[m-1] = p;
-				p = strtok(NULL, " \"");
-			
-			}
-			
-			arg = realloc(arg, sizeof(char*)*(m+1));
-			arg[m] = NULL;
-			
-			printf("%s", arg[0]);
-			if(execvp(arg[0], arg) < 0){
-				printf("ERROR: exec failed\n");
+			execvp("./mycat", "./mycat test1.txt");
+		}
+		else{*/
+		//printf("Hello%s\n", temp);
+		
+		if(strstr(temp, ">") != NULL){
+			//execlp("bash","bash","-c","echo \"int main(){int*segfault=0; *segfault=0;return 0;}\" > badprog.c",(char*)0);
+			//printf("Exec failed\n");
+			//execlp("bash","bash","-c",temp, NULL);
+			pid_t pid = fork();
+			if(pid == -1){
 				exit(1);
 			}
-			//k++;
-			//printf("%d", k);
-		
-		//}
+			if(pid > 0){
+				int status;	
+				waitpid(pid, &status, 0);
+			}
+			if(pid == 0){
 	
+				if(execlp("bash","bash","-c",temp, NULL) < 0){
+					printf("ERROR: exec failed\n");
+					exit(1);
+				}		
+			}
+			
+			//execute(arg);
+		}
+		else{
+			execute(arg);
+		}
+		k++;
 	}
-	*/
+	
+	exit(0);
 	
 	return 0;
 }
@@ -145,6 +146,7 @@ void execute(char ** arg){
 	
 	}
 	if(pid == 0){
+	
 			if(execvp(arg[0], arg) < 0){
 				printf("ERROR: exec failed\n");
 				exit(1);
