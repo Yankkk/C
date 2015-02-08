443c94bd74b14a8d95f0618ac45605d92c1e600e
yangeng2 <yangeng2@a2e70646-4c9d-4ed2-889b-a337aa36896b>
2015-02-08 22:10:27 +0000

    javavsc submission

--- yangeng2/JavaVsC/app/src/main/jni/java_vs_c.c
+++ yangeng2/JavaVsC/app/src/main/jni/java_vs_c.c
@@ -3,11 +3,13 @@
 #include <string.h>
 
 // Todo #1 generate the .h file to get the correct function name:
+/**
 jstring  Java_what_should_go_here_I_wonder_see_hint_below
           (JNIEnv *env, jclass clas)
 {
     return (*env)->NewStringUTF(env,"Hello From C!");
 }
+*/
 
 // You can create function declarations using javah
 // cd into main/java and then just run javah edu.illinois.cs241.javavsc.CGlue to create a .h file
@@ -22,7 +24,7 @@ jstring  Java_what_should_go_here_I_wonder_see_hint_below
 JNIEXPORT jstring JNICALL Java_edu_illinois_cs241_javavsc_CGlue_hello
   (JNIEnv * env, jclass CGlue)
 {
-    return  (*env)->NewStringUTF(env, "Hello world!!!");
+    return  (*env)->NewStringUTF(env, "Hello From C!");
 
 }
 
