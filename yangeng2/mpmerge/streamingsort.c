3a78b286ef7b1a08f18033769bfbc5fe88451bba
yangeng2 <yangeng2@a2e70646-4c9d-4ed2-889b-a337aa36896b>
2015-04-04 18:09:20 +0000

    mp merge

--- yangeng2/mpmerge/streamingsort.c
+++ yangeng2/mpmerge/streamingsort.c
@@ -98,11 +98,11 @@ void create_task(task_t*parent, int start, int end) {
   int mid = ((len/256)/2 + (len/256)%2) * 256;
   */
   int mid = (end+start)/2;
-  int re = mid % 256;
+  int re = mid % 512;
   if(re){
-  	mid = mid - re + 256;
+  	mid = mid - re + 512;
   }
-  if(len > 256) {   //
+  if(len > 512) {   //
     create_task(task,  start,  mid);
     create_task(task,  mid, end);
   } else {
@@ -131,22 +131,22 @@ void do_tasks(int*scratch, task_t* task) {
   
   int len = end - start;
   //int mid = ((len/256)/2 + (len/256)%2)* 256;
+	int mid;
+  if(len <= 512){
+  	mid = (end + start)/2;
+  	int re = mid % 256;
+  	if(re){
+  		mid = mid - re + 256;
+  	}
+  }
+  else{
+  	mid = (end + start)/2;
+  	int re = mid % 512;
+  	if(re){
+  		mid = mid - re + 512;
+  	}
+  }
 
-  int mid = (end + start)/2;
-  int re = mid % 256;
-  if(re){
-  	mid = mid - re + 256;
-  }
-  
-  /*
-  if((len <=256) && (task->parent == NULL)) {
-    qsort(data +start,len,sizeof(int), compare_fns);    
-  } else {
-  	if(!((task->parent != NULL) && (len <= 256))){
-    	simple_merge(data, scratch, start, mid, end);
-    }    
-  }
-  */
   if(len > 256){
   	simple_merge(data, scratch, start, mid, end);
   	if(verbose) 
