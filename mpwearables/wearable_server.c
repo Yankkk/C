175fb66922414bad5f02eede3cd9feeb6a39ac72
yangeng2 <yangeng2@a2e70646-4c9d-4ed2-889b-a337aa36896b>
2015-04-17 20:23:07 +0000

    mp submission

--- mpwearables/wearable_server.c
+++ mpwearables/wearable_server.c
@@ -24,6 +24,9 @@ pthread_mutex_t queue_lock_;
 //a queue for all received data... 
 queue_t receieved_data_;
 
+//
+
+
 
 typedef struct SampleData {
 
@@ -81,48 +84,118 @@ void extract_key(char* line, long* timestamp, SampleData** ret) {
 	sscanf(line, "%zu:%i:%[^:]%:\\.*", timestamp, &((*ret)->data_), (*ret)->type_);
 }
 
-void* wearable_processor_thread(void* args) {
-	int socketfd = *((int*)args);
+unsigned long *data_array;
+int total;
+int num;
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
 
+void* wearable_processor_thread(void* args) {
+	//int socketfd = *((int*)args);
+	int socketfd = (int)args;
 	//Use a buffer of length 64!
 	//TODO read data from the socket until -1 is returned by read
 	
+	//realloc data_array by +1
+	pthread_mutex_lock(&queue_lock_);
+	int current = num;
+	int count=0;
+	data_array=realloc(data_array,sizeof(long)*(num+1));
+	num++;
+	pthread_mutex_unlock(&queue_lock_);
+
 	char buffer[64];
-	while (recev(socketfd, buffer, 64) > 0){
+	while (recv(socketfd, buffer, 64,0) > 0){
+		//printf(" length: %d",length);
 		unsigned long timestamp;
-		SampleData* ret;
-		extract_key(buffer, &timestamp, &ret);
+		SampleData *ret;
+		extract_key(buffer,&timestamp,&ret);
 		pthread_mutex_lock(&queue_lock_);
-		queue_insert(receieved_data_, timestamp, (void*) ret);
-		pthread_mutex_unlock(&queue_lock_)
+		queue_insert(&receieved_data_,(unsigned long)timestamp,ret);
+		total++;
+		pthread_mutex_unlock(&queue_lock_);	
+		data_array[current]=timestamp;
+		// add the currnt entry to the data_array
+		pthread_cond_broadcast(&cv);	
+		count++;
 	}
+	
 	close(socketfd);
+	data_array[current]=-1;
+
+	pthread_cond_broadcast(&cv);
 	return NULL;
 }
 
+int time_send_data(long* data_array,int num, long final_timestamp){
+	int i;
+	for(i=0;i<num;i++){
+		if(data_array[i]!=-1 && data_array[i]<final_timestamp){
+			return 0;
+		} 
+	}
+	return 1;
+}
+
+int selector1(void* entry){
+	SampleData* ret =(SampleData*)entry;
+	if(strcmp(TYPE1,ret->type_)==0){
+		return 1;
+	}
+	return 0;
+}
+
+int selector2(void* entry){
+	SampleData* ret =(SampleData*)entry;
+	if(strcmp(TYPE2,ret->type_)==0){
+		return 1;
+	}
+	return 0;
+}
+
+int selector3(void* entry){
+	SampleData* ret =(SampleData*)entry;
+	if(strcmp(TYPE3,ret->type_)==0){
+		return 1;
+	}
+	return 0;
+}
+
+	
+//pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
+
 void* user_request_thread(void* args) {
 	int socketfd = *((int*)args);
-	int i, ret;
-	int size;
-	timestamp_entry* results;
-
+	//int i, ret;
 	//TODO rread data from the socket until -1 is returned by read
 	//Requests will be in the form
 	//<timestamp1>:<timestamp2>, then write out statiticcs for data between
 	//those timestamp ranges
-	
-	char buffer[200];
-	while(recev(socketfd, buffer, 200) > 0){
+	//
+	//before you send data, check timestamp array
+	char buffer[1024];
+	while(recv(socketfd,buffer,1024,0)>0){
 		unsigned long start;
 		unsigned long end;
-		scanf(buffer, "<%d>:<%d>", &start, &end);
+		sscanf(buffer,"%lu:%lu",&start, &end);
 		pthread_mutex_lock(&queue_lock_);
-		results = queue_gather(&receieved_data_, start, end, compare, &size);
+		while(time_send_data(data_array,num,end)!=1){
+			pthread_cond_wait(&cv,&queue_lock_);
+		}
+		//printf("the size of data: %d\n",total);
+		int size1,size2,size3;
+		timestamp_entry* results1 = queue_gather(&receieved_data_,(unsigned long)start,(unsigned long)end, selector1,&size1);
+		timestamp_entry* results2 = queue_gather(&receieved_data_,(unsigned long)start,(unsigned long)end, selector2,&size2);
+		timestamp_entry* results3 = queue_gather(&receieved_data_,(unsigned long)start,(unsigned long)end, selector3,&size3);
+		write_results(socketfd,TYPE1,results1,size1);
+		write_results(socketfd,TYPE2,results2,size2);
+		write_results(socketfd,TYPE3,results3,size3);
+		write(socketfd,"\r\n",2);
 		pthread_mutex_unlock(&queue_lock_);
+		
+		free(results1);
+		free(results2);
+		free(results3);
 	}
-	write_result(socketfd, TYPE1, results, size);
-	write_result(socketfd, TYPE2, results, size);
-	write_result(socketfd, TYPE3, results, size);
 	close(socketfd);
 	return NULL;
 }
@@ -131,38 +204,47 @@ void* user_request_thread(void* args) {
 //given a string with the port value, set up a 
 //serversocket file descriptor and return it
 int open_server_socket(const char* port) {
-	//TODO
-	int s;
-	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	int s, sock_fd;
+	sock_fd = socket(AF_INET,SOCK_STREAM,0);
+
 	struct addrinfo hints, *result;
-	memset(&hints, 0, sizeof(struct addrinfo));
+	memset(&hints,0,sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
-	
-	s = getaddrinfo(NULL, port, &hints, &result);
-	if(s != 0){
-		fprintf(stderr, "getaddrinfpr: %s\n", gai_strerror(s));
+	s = getaddrinfo(NULL,port,&hints,&result);
+	if(s!=0){
+		fprintf(stderr,"getaddrinfo: %s\n",gai_strerror(s));
+		exit(1);	
+	}
+	if(sock_fd == -1){
+		perror("socket");
 		exit(1);
 	}
-	if(bind(sock_fd, result->ai_addr, result->ai_addrlen) != 0){
-		perror("bind()");
+	int optval = 1;
+	setsockopt(sock_fd,SOL_SOCKET,SO_REUSEPORT,&optval,sizeof(optval));
+	if(bind(sock_fd,result->ai_addr,result->ai_addrlen)!=0){
+		perror("bind");
 		exit(1);
 	}
-	if(listen(sock_fd, 40)!=0){
+	if(listen(sock_fd,40)){
 		perror("listen()");
 		exit(1);
 	}
-
+	free(result);
 	return sock_fd;
 }
 
+int thread_num;
+pthread_t* tid;
+
 void signal_received(int sig) {
 	//TODO close server socket, free anything you dont free in main
+	// free()
 	close(wearable_server_fd);
-	//free();  
-	queue_destroy(&receieved_data_, int free_data);
-	return;
+	pthread_mutex_destroy(&queue_lock_);
+	pthread_cond_destroy(&cv);
+	free(data_array);
 }
 
 int main(int argc, const char* argv[]) {
@@ -170,10 +252,9 @@ int main(int argc, const char* argv[]) {
 		printf("Invalid input size\n");
 		exit(EXIT_FAILURE);
 	}
-
-	//TODO setup sig handler for SIGINT
-	signal(SIGINT, signal_received);
 	
+	//TODO setup sig handler for SIGINT
+	signal(SIGINT,signal_received);	
 	int request_server_fd = open_server_socket(argv[2]);
 	wearable_server_fd = open_server_socket(argv[1]);
 
@@ -184,21 +265,27 @@ int main(int argc, const char* argv[]) {
 
 	queue_init(&receieved_data_);
 	pthread_mutex_init(&queue_lock_, NULL);
-
-	//TODO accept continous requests
-	while(1){
-		int wearable_socket = accept(wearable_server_fd, NULL, NULL);
-		
-		pthread_create(&, NULL, wearable_processor_thread, &wearable_socket);
-	}
 	
-	//TODO join all threads we spawned from the wearables
-	for(){
-		pthread_join();
+	//TODO accept continous requests
+	tid = malloc(1);
+	data_array=malloc(1);
+	while(1){
+		int client_fd = accept(wearable_server_fd,NULL,NULL);
+		if(client_fd ==-1){
+			break;
+		}
+		tid = realloc(tid,sizeof(pthread_t)*(thread_num+1));
+		pthread_create(&tid[thread_num],NULL,wearable_processor_thread,(void*)client_fd);
+		thread_num++;
 	}
-
+	int i;
+	//TODO join all threads we spawned from the wearables
+	for(i=0;i<thread_num;i++){
+		pthread_join(tid[i],NULL);
+	}
+	free(tid);
 	pthread_join(request_thread, NULL);
 	queue_destroy(&receieved_data_, 1);
-
+	
 	return 0;
 }
